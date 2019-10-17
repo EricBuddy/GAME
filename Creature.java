f362049056e6a85b435214b76a36a54f3d3f79cb
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-17 11:07:57 -0400

    Update Creature.java

--- Creature.java
+++ Creature.java
@@ -1,5 +1,12 @@
 package Game;
 
 public interface Creature {
+	String name = null;
+	public void attack(Creature creature);
+	public void takeDamage(int damage);
+	public boolean isAlive();
+	@Override
+	public String toString();
+
 
 }
