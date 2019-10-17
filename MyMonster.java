d97e2f355c9c445b89ccc0bbdd0d93f9c9b75399
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-17 14:30:00 -0400

    Update MyMonster.java

--- MyMonster.java
+++ MyMonster.java
@@ -1,30 +1,23 @@
 package Game;
 
-public abstract class MyMonster extends MyCreature implements Monster {
+public  class MyMonster extends MyCreature implements Monster {
 	private int enrageThreshold;
-	//protected damage ;
 	public MyMonster(String name, String description, int hitPoints, int damage, int enrageThreshold) {
 		super(name, description, hitPoints, damage);
 		this.enrageThreshold = enrageThreshold;
 	}
 	public boolean canEnrage() {
-		if( this.hitPoints < this.enrageThreshold) {
+		if( getHitPoints()/*this.hitPoints*/ < this.enrageThreshold) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	public void enrage() {
-		if(canEnrage() = true) {
+		if(canEnrage() == true) {
 			setDamage(getDamage()*2);
 		} else {
-			canEnraged() = false;
-		}
-		System.out.println(MyMonster.name + " has been enraged");
+			canEnrage() == false;
+			}
+		System.out.println(getName() + " has been enraged");
 	}
-	
-	}
-	
-	
-
-}
