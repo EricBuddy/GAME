8f199a3fdf35777d213da305ba74807c9a0b9da8
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-17 11:06:47 -0400

    Update MyBattle.java

--- MyBattle.java
+++ MyBattle.java
@@ -1,5 +1,13 @@
 package Game;
 
-public class MyBattle {
+public abstract class MyBattle  implements Battle{
+	private MyPlayer player;
+	private Monster monster;
+
+	public MyBattle( Player player, Monster monster) {
+		this.player= (MyPlayer) player;
+		this.monster=(MyMonster)monster;
+	}
+	
 
 }
