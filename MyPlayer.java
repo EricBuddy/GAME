5b7b0e9021a80fbb01b76bdf1e42a6ed26ecc9b1
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-17 11:09:29 -0400

    Update MyPlayer.java

--- MyPlayer.java
+++ MyPlayer.java
@@ -1,5 +1,20 @@
 package Game;
 
-public class MyPlayer {
+public class MyPlayer extends MyCreature implements Player{
+	private String name;
+	private String description;
+	private int hitPoints;
+	private int damage;
+	private int healAmount;
+
+	public MyPlayer(String name, String description, int hitPoints, int damage, int healAmount ){
+		super(name, description, hitPoints, damage);
+		this.healAmount = healAmount;
+	}
+	public void heal() {
+		setHitPoints(getHitPoints()+ healAmount);
+		//this.hitPoints + this.healAmount= hitPoints;
+		System.out.print(this.name + " is healed by " + healAmount );
+	}
 
 }
