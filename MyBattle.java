29c1e15999d7eadca664c5957c973107bb9400c6
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-20 06:35:02 -0400

    Update MyBattle.java

--- MyBattle.java
+++ MyBattle.java
@@ -1,13 +1,45 @@
 package Game;
 
-public abstract class MyBattle  implements Battle{
-	private MyPlayer player;
+import java.util.Scanner;
+
+public class MyBattle extends java.lang.Object implements Battle{
+	private Player player;
 	private Monster monster;
-
-	public MyBattle( Player player, Monster monster) {
-		this.player= (MyPlayer) player;
-		this.monster=(MyMonster)monster;
-	}
 	
+	public MyBattle(Player player, Monster monster) {
+		this.player = player;
+		this.monster = monster;
+	}
+	@Override
+	public void run() {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Battle begins: " + player.getName() + " vs. " + monster.getName());
+		System.out.println("Player HP: " + player.getHitPoints() + "\n Monster HP: " + monster.getHitPoints());
+		while(player.isAlive() && monster.isAlive()) {
+			System.out.println("Player's turn: attack (a) or heal (h)?");
+			char input = scnr.next().charAt(0);
+			if(input == 'a') {
+				player.attack(monster);
+				monster.takeDamage(player.getDamage());
+				monster.isAlive();
+			}
+			else if (input == 'h') {
+				player.heal();
+				monster.isAlive();
+			}
+			System.out.println("Monster's turn:");
+			if(monster.canEnrage() == true) {
+				monster.enrage();
+			}
+			monster.attack(player);
+			player.takeDamage(monster.getDamage());
+			if(!monster.isAlive()) {
+			System.out.println(monster.getName() + " is dead");
+			} else {
+			System.out.println("Player HP: " + player.getHitPoints() + " \n Monster HP: " + monster.getHitPoints());
+			}
 
+		}
+		
+	}
 }
