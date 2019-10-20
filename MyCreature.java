345a12e0f69c90a469745a541739d5dc3168af33
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-20 06:35:50 -0400

    Update MyCreature.java

--- MyCreature.java
+++ MyCreature.java
@@ -6,48 +6,52 @@ public class MyCreature extends java.lang.Object implements Creature{
 	private int hitPoints;
 	private int damage;
 	private Room room;
-
-	public MyCreature(String name, String description, int hitPoints, int damage){
+	
+	
+	public MyCreature(String name, String description, int hitPoints, int damage) {
 		this.name = name;
-		this.description=description;
-		this.hitPoints=hitPoints;
-		this.damage=damage;
-		this.room=null;
+		this.description = description;
+		setHitPoints(hitPoints);
+		setDamage(damage);
+		this.room = null;	
 	}
-	public void setName(String name) {
-		this.name=name;
-	}
-	public void setDescription(String description) {
-		this.description=description;
-	}
-	public void setHitPoints( int hitPoints) {
-		this.hitPoints=hitPoints;
-	}
-	public void setDamage(int damage) {
-		this.damage=damage;
-	}
-	public String getName(){
+	
+	public String getName() {
 		return name;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public int getHitPoints() {
 		return hitPoints;
 	}
-	public void setRoom(Room room) {
-		this.room=room;
+	
+	public int getDamage() {
+		return damage;
 	}
+	
 	public Room getRoom() {
 		return room;
 	}
-	public boolean isAlive() {
-		if(hitPoints > 0) {
-			return true;
-		} else {
-			return false;
-		}
+	
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
 	}
+	
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public void attack(Creature creature) {
+		System.out.println(getName() + " attacks "  + creature.name + ", dealing " + damage + " damage");
+	}
+			
 	public void takeDamage(int damage) {
 		if(hitPoints - damage >= 0) {
 			hitPoints -= damage; 
@@ -55,8 +59,14 @@ public class MyCreature extends java.lang.Object implements Creature{
 			 hitPoints = 0;
 		}
 	}
-	public void attack(Creature creature) {
-		System.out.println(this.name + " attacks "  + creature + ", dealing " + damage + " damage");
+	
+
+	public boolean isAlive() {
+		if(hitPoints > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
@@ -64,5 +74,5 @@ public class MyCreature extends java.lang.Object implements Creature{
 		String s = String.format("name %15s, description %20s, hitPoints %03d, damage %02d", name, description, hitPoints, damage);
 		return s;
 	}
-
+	
 }
