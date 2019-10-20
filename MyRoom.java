adbcb0434e66e5544822c037710e749bacf9b261
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-20 06:37:49 -0400

    Update MyRoom.java

--- MyRoom.java
+++ MyRoom.java
@@ -4,24 +4,41 @@ public class MyRoom extends java.lang.Object implements  Room{
 	private int roomIndex;
 	private String description;
 	private Monster monster;
-	public MyRoom(int roomIndex, String description, Monster monster ) {
-		this.roomIndex= roomIndex;
-		this.description=description;
-		this.monster=monster;
+
+	public MyRoom(int roomIndex, String description, Monster monster) {
+		this.roomIndex = roomIndex;
+		this.description = description;
+		this.monster = monster;
 	}
+		
 	public boolean isComplete() {
-		if(isAlive()== true){
-		    
+		if(!monster.isAlive()) {
+			return true;
+		} else {
+			return false;
 		}
-		
-		
 	}
+
 	public void enter(Player player) {
-		System.out.println(player+ "This room is "+ this.description4+ ", and has "+ this.monster);
+		System.out.println(player.getName() + " enters room " + roomIndex);
+		System.out.println("This Room is " + description + ", and has monster " + monster);
+		if(monster.isAlive()) {
+			MyBattle b1 = new MyBattle(player, monster);
+			b1.run();
+			
+		} else {
+			System.out.println(monster.getName() + " is dead");
+			isComplete();
+		}
+
 	}
+	
 	@Override
 	public String toString() {
-		String s = String.format("Room index %15s, description %20s, monster name %20s", roomIndex, description, monster);
+		String s = String.format("Room index %2d, description %20s, monster name %20s", roomIndex, description, monster);
 		return s;
 	}
+	
+
+
 }
