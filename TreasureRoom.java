c0ad7f118bc0cfbfc3d0ded2cd04121c76f98ddc
EricBuddy <56688836+EricBuddy@users.noreply.github.com>
2019-10-21 22:22:31 -0400

    Update TreasureRoom.java

--- TreasureRoom.java
+++ TreasureRoom.java
@@ -1,7 +1,9 @@
 package Game;
 
+import java.io.PrintWriter;
+
 public interface TreasureRoom extends Room {
-	@Override
-	public void enter(MyPlayer player);
+	
+	public void enter(MyPlayer player,MyMonster monster,PrintWriter pw);
 	
 }
